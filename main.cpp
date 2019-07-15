f744e44f9e648009a5b7e125ffc7a26477e8faa8
Pavan Seshadri <32950471+pseshadri9@users.noreply.github.com>
2019-07-14 21:59:04 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include "tgaimage.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+int main(int argc, char** argv) {
+        TGAImage image(100, 100, TGAImage::RGB);
+        image.set(52, 41, red);
+        image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        image.write_tga_file("output.tga");
+        return 0;
+}
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
+    bool steep = false; 
+    if (std::abs(x0-x1)<std::abs(y0-y1)) { 
+        std::swap(x0, y0); 
+        std::swap(x1, y1); 
+        steep = true; 
+    } 
+    if (x0>x1) { 
+        std::swap(x0, x1); 
+        std::swap(y0, y1); 
+    } 
+    int dx = x1-x0; 
+    int dy = y1-y0; 
+    int derror2 = std::abs(dy)*2; 
+    int error2 = 0; 
+    int y = y0; 
+    for (int x=x0; x<=x1; x++) { 
+        if (steep) { 
+            image.set(y, x, color); 
+        } else { 
+            image.set(x, y, color); 
+        } 
+        error2 += derror2; 
+        if (error2 > dx) { 
+            y += (y1>y0?1:-1); 
+            error2 -= dx*2; 
+        } 
+    } 
+} 
